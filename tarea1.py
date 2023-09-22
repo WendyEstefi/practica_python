61d6c5cf39742005ed452aefdc63bb1a22c39fc8
WendyEstefi <wendyestefi1999@gmail.com>
2023-09-22 17:38:32 -0500

    Creación de Script

---
+++ tarea1.py
@@ -0,0 +1,7 @@
+# Cargue un paquete de su elección 
+import scikit-learn as scl
+#Dos variables, una que contenga su nombre y la otra su edad
+nombre = 'Wendy'
+edad = 23
+# Crear una oración que indique su nombre y su edad utilizando las dos variables.
+print('Mi nombre es',nombre, 'y mi edad es', edad)
